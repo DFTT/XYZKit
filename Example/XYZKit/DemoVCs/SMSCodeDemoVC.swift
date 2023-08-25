c15ed773e3d4103218f981995386157a7caa3bc9
大大东 <lidong@021.com>
2023-08-25 18:12:56 +0800

    add KeyBoardObserver

--- Example/XYZKit/DemoVCs/SMSCodeDemoVC.swift
+++ Example/XYZKit/DemoVCs/SMSCodeDemoVC.swift
@@ -15,38 +15,42 @@ class SMSCodeDemoVC: UIViewController {
         
         self.view.bgColor(.white)
         
-        
         let config1 = XYZSMSCodeInputView.Configs.default()
         let view1 = XYZSMSCodeInputView(config1)
         view1.frame = CGRect(x: 50, y: 200, width: 300, height: 60)
         view.addSubview(view1)
-        view1.inputCompletion = { code in
+        view1.inputCompletion = { _ in
             // 重置输入
             view1.resetInput()
         }
-        
         
         var config2 = XYZSMSCodeInputView.Configs.default()
         config2.codeBoder = .roundRect
         let view2 = XYZSMSCodeInputView(config2)
         view2.frame = CGRect(x: 50, y: 300, width: 300, height: 60)
         view.addSubview(view2)
-        view2.inputCompletion = { code in
+        view2.inputCompletion = { _ in
             // 收键盘
             view2.endEditing(true)
         }
 
-        
         var config3 = XYZSMSCodeInputView.Configs.default()
         config3.codeBoder = .rect
         config3.font = 23.fontBold
         let view3 = XYZSMSCodeInputView(config3)
         view3.frame = CGRect(x: 50, y: 400, width: 300, height: 60)
         view.addSubview(view3)
-        view3.inputCompletion = { code in
+        view3.inputCompletion = { _ in
             // 收键盘
             view3.endEditing(true)
         }
+        
+        KeyBoardObserver.addLisenner(self)
     }
-    
+}
+
+extension SMSCodeDemoVC: KeyBoardObserverProtocol {
+    func keyboardWillChange(toRect: CGRect, toShow: Bool, duration: TimeInterval) {
+        print("\(toShow) \(duration) \(toRect)")
+    }
 }
---
+++ XYZKit/Classes/Core/KeyBoardObserver.swift
@@ -0,0 +1,54 @@
+//
+//  KeyBoardObserver.swift
+//  XYZKit
+//
+//  Created by 大大东 on 2023/8/25.
+//
+
+import UIKit
+
+public protocol KeyBoardObserverProtocol {
+    func keyboardWillChange(toRect: CGRect, toShow: Bool, duration: TimeInterval)
+}
+
+public class KeyBoardObserver: NSObject {
+    public typealias T = KeyBoardObserverProtocol & UIResponder
+
+    public static func addLisenner(_ ner: T) {
+        KeyBoardObserver.default.lisenners.add(ner)
+    }
+
+    public static func removeLisenner(_ ner: T) {
+        self.default.lisenners.remove(ner)
+    }
+
+    static let `default` = KeyBoardObserver()
+
+    var lisenners = NSHashTable<AnyObject>.weakObjects()
+
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbWillHidden(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func kbWillShow(_ notify: Notification) {
+        if let kbToRect = notify.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+           let duration = notify.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval
+        {
+            lisenners.objectEnumerator().forEach { ins in
+                (ins as! KeyBoardObserverProtocol).keyboardWillChange(toRect: kbToRect, toShow: true, duration: duration)
+            }
+        }
+    }
+
+    @objc private func kbWillHidden(_ notify: Notification) {
+        if let kbToRect = notify.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+           let duration = notify.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval
+        {
+            lisenners.objectEnumerator().forEach { ins in
+                (ins as! KeyBoardObserverProtocol).keyboardWillChange(toRect: kbToRect, toShow: false, duration: duration)
+            }
+        }
+    }
+}
