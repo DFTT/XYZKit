4d12cf381aee9ae948eb97a5a51dd9062b0ceaaf
大大东 <lidong@021.com>
2024-02-20 16:28:42 +0800

    0.7.0

---
+++ XYZKit/Classes/Core/XYZUIKit/UIApplication+XYZ.swift
@@ -0,0 +1,31 @@
+//
+//  UIApplication+XYZ.swift
+//  XYZKit
+//
+//  Created by 大大东 on 2024/2/20.
+//
+
+import Foundation
+
+public extension UIApplication {
+    static func topViewController() -> UIViewController? {
+        guard let del = UIApplication.shared.delegate,
+              let root = del.window??.rootViewController else { return nil }
+
+        var topVc: UIViewController? = root
+        var irCount = 0
+        while irCount < 10 {
+            irCount += 1
+            if let vc = topVc?.presentedViewController {
+                topVc = vc
+            } else if let nav = topVc as? UINavigationController {
+                topVc = nav.topViewController
+            } else if let tab = topVc as? UITabBarController {
+                topVc = tab.selectedViewController
+            } else {
+                break
+            }
+        }
+        return topVc
+    }
+}
